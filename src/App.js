e67327c2c8afa0bb2403400df85757acf884ef2f
Purritzo <xxpurelistxx@gmail.com>
2022-05-19 23:12:58 +0800

    Updated App.js to import name prompt form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import Hangman from './components/Hangman.js'
+//import logo from './logo.svg';
+import NameForm from './Zach-part.js';
 
 function App() {
   return (
     <div className="App">
-        <Hangman />
+        <NameForm />
     </div>
   );
 }
