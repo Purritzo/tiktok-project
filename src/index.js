b7e3401f429b48bb65d07b80477bb2d5a2cdfdd6
Purritzo <xxpurelistxx@gmail.com>
2022-05-27 16:19:26 +0800

    taking out redux temp

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './src/app/store.js';
+import storeJs from './store,js';
 import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  //<Provider store={storeJs}>
     <App />
-  </Provider>
+  //</Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
