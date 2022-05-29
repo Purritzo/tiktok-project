e3b4135ad86e0617fbd72bd75f0f164be5d98de2
Purritzo <xxpurelistxx@gmail.com>
2022-05-29 22:48:37 +0800

    removed redundant files

--- src/components/functions/handleChange.js
+++
@@ -1,4 +0,0 @@
-//this function changes value property as the input field changes (is this needed?)
-export default function handleChange(event) {
-	this.setState({value: event.target.value});
-}
--- src/components/functions/handleSubmit.js
+++
@@ -1,9 +0,0 @@
-//this function stores the submitted word, and creates the display that will be shown to the player
-export default function handleSubmit(event) {
-  this.setState({
-    word: this.state.value,
-    word_display: Array(this.state.value.length).fill('_ '),
-    stage : 2
-  })
-  event.preventDefault();
-}
